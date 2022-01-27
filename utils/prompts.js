a6fd11a48290d74ce0e5271150dda497da4b7d7b
amarten <alexdmarten@gmail.com>
2022-01-27 09:54:29 -0800

    initial prompts for what the user would like to do first

---
+++ utils/prompts.js
@@ -0,0 +1,15 @@
+const { listen } = require('express/lib/application');
+const inquirer = require('inquirer');
+
+const initPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'initPrompt',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ]);
+};
+
+initPrompt();
