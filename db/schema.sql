997b62086245fad243f675c2f3f9bc55fa97d1cf
amarten <alexdmarten@gmail.com>
2022-01-29 12:29:41 -0800

    setup schema.sql with employees, roles, and departments tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,23 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    role_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary INTEGER NOT NULL,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
-)
+    salary INTEGER NOT NULL,
+    managers VARCHAR(30) NOT NULL
+);
